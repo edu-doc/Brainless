92d2f23d7d2d05e4975a39e82b9389d4c602bb4c
Antonio Eduardo <antonioeduardooc@gmail.com>
2024-08-02 14:35:21 -0300

    First Commit

---
+++ src/main/java/com/elite/brainless/BrainlessApplication.java
@@ -0,0 +1,13 @@
+package com.elite.brainless;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrainlessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrainlessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elite/brainless/BrainlessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elite.brainless;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrainlessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
