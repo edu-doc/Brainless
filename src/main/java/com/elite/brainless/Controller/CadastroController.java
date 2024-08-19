e57cb0771b4df71b5ed6a80d383165b3ec26da47
Antonio Eduardo <antonioeduardooc@gmail.com>
2024-08-19 16:10:03 -0300

    Retirada Entity Cadastro

--- src/main/java/com/elite/brainless/Controller/CadastroController.java
+++ src/main/java/com/elite/brainless/Controller/CadastroController.java
@@ -1,6 +1,5 @@
 package com.elite.brainless.Controller;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
