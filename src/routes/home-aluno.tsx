bd0a79abedd4ba0bb1c4e32534b9019fece02a95
Eduardo <eduardopazvieiraa@gmail.com>
2024-09-22 22:35:08 -0300

    alteraçao na tabela dos alunos

--- src/routes/home-aluno.tsx
+++ src/routes/home-aluno.tsx
@@ -7,7 +7,8 @@ interface Questao {
     id: number;
     enunciado: string;
     tema: string[]; 
-    ano: number; 
+    ano: number;
+    isPublica: boolean; 
 }
 
 
@@ -43,7 +44,7 @@ const HomeAluno = () => {
             <>
             <div className="flex flex-col h-full">
                 <NavBar />
-                <div className="bg-blue-500 flex flex-1 justify-around">
+                <div className="bg-gradient-to-b from-[#007BFF] from-41% to-[#0056B3] to-90% flex flex-1 justify-around">
                     
                     {/* Menu Lateral */}
                     <div className=" w-2/12 m-4 bg-gray-800 text-white p-4 rounded-lg shadow-lg">
@@ -57,13 +58,7 @@ const HomeAluno = () => {
                     <div className="p-4 w-10/12"> {/* Ajuste ml-72 para a largura do menu e mt-16 para a altura da barra de navegação */}
                         <main className="flex-1 bg-white h-full rounded-lg shadow-lg p-6">
                             <div className="flex justify-between items-center mb-3">
-                                <h1 className="text-2xl font-bold mb-4">Tabela de Dados</h1>
-                                <button
-                                    onClick={() => navigate("/cadastrar-questao")}
-                                    className="flex w-2/12 justify-center rounded-md bg-[#0056B3] p-2 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-[#007BFF] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                                >
-                                    Cadastrar Questão
-                                </button>
+                                <h1 className="text-2xl font-bold mb-4">Últimas Questões</h1>
                             </div>
                                 
                             {/* Tabela */}
@@ -74,12 +69,12 @@ const HomeAluno = () => {
                                             <th className="px-4 py-2">Enunciado</th>
                                             <th className="px-4 py-2">Tema</th>
                                             <th className="px-4 py-2">Ano</th>
-                                            <th className="px-4 py-2">Editar</th>
-                                            <th className="px-4 py-2">Excluir</th>
+                                            <th className="px-4 py-2">Responder</th>
                                         </tr>
                                     </thead>
                                     <tbody>
                                         {questoes.map((questao) => (
+                                            questao.isPublica && (
                                             <tr key={questao.id} className="border-b bg-gray-300">
                                                 <td className="px-4 py-2">{questao.enunciado}</td>
                                                 <td>{(questao.tema ?? []).join(', ')}</td> {/* Usando coalescência nula */}
@@ -90,19 +85,12 @@ const HomeAluno = () => {
                                                 <td className="px-4 py-2">
                                                     <div className="flex justify-center">
                                                         <button className="p-2 bg-[#0056B3] w-8 h-8 text-white rounded-md flex items-center justify-center hover:bg-[#007BFF] ">
-                                                            <img src="src/assets/icon-editar.png" alt="editar" className="w-full h-full filter invert drop-shadow-md"/>
+                                                            <img src="src/assets/icon-responder.png" alt="responder" className="w-full h-full filter invert drop-shadow-md"/>
                                                         </button> 
                                                     </div>
                                                 </td>
-                                                <td className="px-4 py-2 flex justify-center">
-                                                    <div className="flex justify-center">
-                                                        <button className="p-2 bg-red-600 w-8 h-8 drop-shadow-md text-white rounded-md flex items-center justify-center hover:bg-red-500">
-                                                            <img src="src/assets/icon-excluir.png" alt="excluir" className="w-full h-full filter invert drop-shadow-md"/>
-                                                        </button>
-                                                    </div>
-                                                </td>
                                             </tr>
-                                        ))}
+                                        )))}
                                     </tbody>
                                 </table>
                             </div>
