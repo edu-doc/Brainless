1666ef89e8af26259c8f7428d70c48173ef51e20
Antonio Eduardo <antonioeduardooc@gmail.com>
2024-09-23 11:59:27 -0300

    Update Delete

--- brainless-backend/src/main/java/com/elite/brainless/Controller/QuestaoController.java
+++ brainless-backend/src/main/java/com/elite/brainless/Controller/QuestaoController.java
@@ -1,12 +1,12 @@
 package com.elite.brainless.Controller;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.elite.brainless.Model.Entity.Questao;
-import com.elite.brainless.Model.Entity.QuestaoResponse;
 import com.elite.brainless.Model.Service.QuestaoService;
 
 import jakarta.validation.Valid;
@@ -58,6 +57,23 @@ public class QuestaoController {
     public Optional<Questao> SearchQuest(Long id){
         Optional<Questao> questList = service.findById(id);
         return questList;
+    }
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @DeleteMapping
+    public ResponseEntity<String> DeleteQuest(Long id){
+
+        Optional<Questao> questList = service.findById(id);
+
+        if (questList == null){
+            return ResponseEntity.status(HttpStatus.CREATED)
+                .body("Questão não encontrada");
+        }
+        
+        service.deleteById(id);
+        
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body("Questão deletada com sucesso");
     }
 
 }
