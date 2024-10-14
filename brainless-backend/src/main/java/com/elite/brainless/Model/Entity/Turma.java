71e3e90dd7785fc562191495c36f2952900b052c
Eduardo <eduardopazvieiraa@gmail.com>
2024-10-14 01:19:29 -0300

    tirando import desnecessario

--- brainless-backend/src/main/java/com/elite/brainless/Model/Entity/Turma.java
+++ brainless-backend/src/main/java/com/elite/brainless/Model/Entity/Turma.java
@@ -6,7 +6,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
