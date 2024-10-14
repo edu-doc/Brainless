5499f35f2680aabace0df4906ab5edec11ed6aeb
Eduardo <eduardopazvieiraa@gmail.com>
2024-10-14 01:19:13 -0300

    colocando novos atributos na response

--- brainless-backend/src/main/java/com/elite/brainless/Model/Entity/QuestaoResponse.java
+++ brainless-backend/src/main/java/com/elite/brainless/Model/Entity/QuestaoResponse.java
@@ -2,8 +2,12 @@ package com.elite.brainless.Model.Entity;
 
 import java.util.List;
 
-public record QuestaoResponse(Long id ,List<String> alternativas , String enunciado, String justificativa, String resposta , List<String> tema , Integer ano, Boolean isPublica) {
-    public QuestaoResponse(Questao questao){
-        this(questao.getId() , questao.getAlternativas(), questao.getEnunciado(), questao.getJustificativa(), questao.getResposta(), questao.getTema() , questao.getAno(), questao.getIsPublica());
+public record QuestaoResponse(Long id, List<String> alternativas, String enunciado, String justificativa,
+        String resposta, List<String> tema, Integer ano, Boolean isPublica, String semestre, String turma,
+        String atividade) {
+    public QuestaoResponse(Questao questao) {
+        this(questao.getId(), questao.getAlternativas(), questao.getEnunciado(), questao.getJustificativa(),
+                questao.getResposta(), questao.getTema(), questao.getAno(), questao.getIsPublica(),
+                questao.getSemestre(), questao.getTurma(), questao.getAtividade());
     }
 }
